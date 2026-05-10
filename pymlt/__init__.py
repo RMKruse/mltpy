4ac03242708edc592b4af3464f8c4a606e6cc34d
RMKruse <rene.marcel.kruse@protonmail.com>
2026-05-10 17:09:09 +0200

    added current version in init. #31

--- pymlt/__init__.py
+++ pymlt/__init__.py
@@ -20,7 +20,7 @@ from pymlt.optimizer import OptimizerConfig
 from pymlt.tram import BoxCox, Colr, Coxph, Lm, Polr
 from pymlt.variables import CensoredData, CensoringType, OrderedVariable
 
-__version__ = "0.1.0"
+__version__ = "0.3.0"
 
 __all__ = [
     # Models
